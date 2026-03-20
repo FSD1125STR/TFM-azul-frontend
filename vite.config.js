d835b8581c5a63e881a542921778b7bbb856ed42
samuelpampi <samuelpampillonroa@gmail.com>
2026-03-20 20:35:31 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5288,
-    strictPort: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3301",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
+})
