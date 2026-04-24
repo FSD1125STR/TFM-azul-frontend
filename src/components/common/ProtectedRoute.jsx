49371e6b80271ccbb2a9fc1f172df482db615423
samuelpampi <samuelpampillonroa@gmail.com>
2026-04-24 15:39:15 +0200

    mejora del join crew si no existe la crew por error de token

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ export const ProtectedRoute = ({ children }) => {
     const { isLoggedIn, user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading) return null;
+    if(loading) return <div style={{ visibility: "hidden" }} />;
 
     if(!user || !isLoggedIn) {
         //Guardamos la url original para saber a donde redireccionar despues del login y la metemos como parametro next
